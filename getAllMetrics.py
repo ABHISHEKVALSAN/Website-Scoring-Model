2edda87a4437021aedbeddeb280cc6c6aab67cd6
ABHISHEKVALSAN <abhishekvalsan9999@gmail.com>
2019-05-02 02:32:32 +0530

    Correction of metric required.

--- getAllMetrics.py
+++ getAllMetrics.py
@@ -274,7 +274,7 @@ def getMetrics(num,url):
 	return
 def main(filename):
 	fields			= ["slno","url","p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13"]
-	csvFilename		= "temp.csv"
+	csvFilename		= "tempUrlMetrics.csv"
 	csvFile			= open(csvFilename,"a+")
 	csvWriter		= csv.writer(csvFile)
 	csvWriter.writerow(fields)
